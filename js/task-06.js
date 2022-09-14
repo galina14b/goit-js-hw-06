73eca0b1b8fe8ac56fa7c7818815796670e8e02d
Halyna <galina14b@gmail.com>
2022-09-14 18:52:26 +0300

    fixed task-6

--- js/task-06.js
+++ js/task-06.js
@@ -1,7 +1,10 @@
 const inptEl = document.querySelector('#validation-input');
 inptEl.addEventListener('blur', checkInputText);
+inptEl.addEventListener('focus', () => inptEl.value = '')
 
 function checkInputText(event) {
+  this.classList.remove('valid');
+  this.classList.remove('invalid');
   if (event.currentTarget.value.length == event.currentTarget.dataset.length) {
     this.classList.add('valid');
   } else {
